4c6298c3576119012f09481a881d06035b6a2982
Jordan C <jordan.c4922@gmail.com>
2025-02-10 14:37:28 -0700

    Initial Commit

---
+++ SwiftNesFrontend/SwiftNesFrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNesFrontendApp.swift
+//  SwiftNesFrontend
+//
+//  Created by Jordan Christensen on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNesFrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftNesFrontendTests/SwiftNesFrontendTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftNesFrontendTests.swift
+//  SwiftNesFrontendTests
+//
+//  Created by Jordan Christensen on 2/10/25.
+//
+
+import Testing
+
+struct SwiftNesFrontendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
